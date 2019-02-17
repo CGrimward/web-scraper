19d22a7382335bd4cb25f643f2334a5c01fa6915
CGrimward <chris.grimward.dev@gmail.com>
2019-02-17 21:35:51 +0000

    Initial Project structure

---
+++ src/test/java/scraper/PageScraperTest.java
@@ -0,0 +1,22 @@
+package scraper;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PageScraperTest {
+
+
+    @Test
+    public void getChildPagesFromUrl_whenValidUrl_thenReturnValidChildUrls() {
+
+    }
+}
