cb2f1834b8958127ffba78dc3e37b75b9bda00b7
Chris Grimward <ChrisGrimward@rentalcars.com>
2019-02-18 23:20:42 +0000

    Added Failing test case for orchestration of pageScraper. Added Mockito dependency for testing

--- src/main/java/service/PageService.java
+++ src/main/java/service/PageService.java
@@ -1,4 +1,12 @@
 package service;
 
+import domain.Product;
+
+import java.util.List;
+
 public class PageService {
+
+    public List<Product> extractProductsFromPage(String url) {
+        return null;
+    }
 }
---
+++ src/test/java/service/PageServiceTest.java
@@ -0,0 +1,51 @@
+package service;
+
+import domain.Product;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import scraper.PageScraper;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PageServiceTest {
+
+    @Mock
+    private PageScraper pageScraper;
+
+    @InjectMocks
+    private PageService pageService;
+
+
+    @Test
+    public void extractProductsFromUrl(String url) throws IOException {
+        List<String> productUrls = Arrays.asList("ProductUrlOne", "ProductUrlTwo", "ProductUrlThree");
+        Product productOne = new Product("ProductOne", "ProductOne", new BigDecimal(1.00), 1);
+        Product productTwo = new Product("ProductTwo", "ProductTwo", new BigDecimal(2.00), 2);
+        Product productThree = new Product("ProductThree", "ProductThree", new BigDecimal(3.00), 3);
+
+        when(pageScraper.getProductLinksFromUrl(any())).thenReturn(productUrls);
+        when(pageScraper.getProductFromUrl("ProductUrlOne")).thenReturn(productOne);
+        when(pageScraper.getProductFromUrl("ProductUrlTwo")).thenReturn(productTwo);
+        when(pageScraper.getProductFromUrl("ProductUrlThree")).thenReturn(productThree);
+
+        List<Product> products = pageService.extractProductsFromPage("ValidUrl");
+
+        assertThat(products).contains(productOne, productTwo, productThree);
+
+    }
+
+
+
+
+}
