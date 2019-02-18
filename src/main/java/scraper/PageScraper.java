881d5dd52e7aa6a992ddb0a4f712e41e57178078
Chris Grimward <ChrisGrimward@rentalcars.com>
2019-02-18 20:43:44 +0000

    Method to Gather product links from page + tests

--- src/main/java/scraper/PageScraper.java
+++ src/main/java/scraper/PageScraper.java
@@ -1,5 +1,21 @@
 package scraper;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PageScraper {
-    
+
+    public List<String> getProductLinksFromUrl(String url) throws IOException {
+        Document document = Jsoup.connect(url).get();
+        Elements productNames = document.getElementsByAttributeValue("class", "productNameAndPromotions");
+        return productNames.stream()
+                .map(name -> name.getElementsByTag("a"))
+                .map(aTag -> aTag.attr("abs:href"))
+                .collect(Collectors.toList());
+    }
 }
--- src/test/java/scraper/PageScraperTest.java
+++ src/test/java/scraper/PageScraperTest.java
@@ -1,22 +1,22 @@
 package scraper;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PageScraperTest {
 
+    private PageScraper scraper = new PageScraper();
 
     @Test
-    public void getChildPagesFromUrl_whenValidUrl_thenReturnValidChildUrls() {
+    public void getProductLinksFromUrl_whenValidUrl_thenReturnValidProductUrls() throws IOException {
+        String url = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
 
+        List<String> productLinks = scraper.getProductLinksFromUrl(url);
+
+        assertThat(productLinks).hasSize(17);
     }
 }
