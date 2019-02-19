f67fbf8650f86d7974f8871c062c8cfce1d782ab
Chris Grimward <ChrisGrimward@rentalcars.com>
2019-02-19 02:52:13 +0000

    Removed some unused setters

--- src/main/java/uk/co/sainsburys/exercise/domain/Product.java
+++ src/main/java/uk/co/sainsburys/exercise/domain/Product.java
@@ -20,31 +20,15 @@ public class Product {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public BigDecimal getPricePerUnit() {
         return pricePerUnit;
-    }
-
-    public void setPricePerUnit(BigDecimal pricePerUnit) {
-        this.pricePerUnit = pricePerUnit;
     }
 
     public Integer getKcalPer100g() {
         return kcalPer100g;
     }
 
-    public void setKcalPer100g(Integer kcalPer100g) {
-        this.kcalPer100g = kcalPer100g;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
